d194168c9eb6340b0d14c44d718a0e361de26b2f
OussamaDjebbour <oussama_djebbour@outlook.com>
2024-08-02 17:43:39 +0100

    mobile

--- src/features/AppLayout.tsx
+++ src/features/AppLayout.tsx
@@ -3,11 +3,11 @@ import ButtonStep from '../ui/ButtonStep';
 import FormContainer from './FormContainer';
 
 function AppLayout() {
-  const [step, setStep] = useState<number>(1);
+  const [step, setStep] = useState<number>(2);
 
   return (
-    <main className="bg-image-mobile mb-10 flex flex-col items-center justify-between bg-contain bg-no-repeat">
-      <ul className="my-8 flex justify-center gap-4">
+    <main className="mb-10 flex flex-col items-center justify-between bg-image-mobile bg-contain bg-no-repeat md:w-[95%] md:flex-row md:bg-image-desktop">
+      <ul className="my-8 flex justify-center gap-4 md:flex-col">
         <ButtonStep content={1} step={step} />
         <ButtonStep content={2} step={step} />
         <ButtonStep content={3} step={step} />
--- src/ui/Footer.tsx
+++ src/ui/Footer.tsx
@@ -36,5 +36,32 @@ function Footer({ step, prevStep, handleFormClick }: FooterProps) {
     document.body,
   );
 }
+// function Footer({ step, prevStep, handleFormClick }: FooterProps) {
+//   return (
+//     <footer
+//       // className="mt-auto flex w-full items-center justify-between bg-white p-5"
+//       className="absolute bottom-0 right-0 flex w-full items-center justify-between bg-white p-5"
+//     >
+//       {step > 1 ? (
+//         <button
+//           type="button"
+//           onClick={() => prevStep()}
+//           className="font-[500] text-cool-gray hover:cursor-pointer hover:text-marine-blue"
+//         >
+//           Go back
+//         </button>
+//       ) : (
+//         <span></span>
+//       )}
+//       <button
+//         type="submit"
+//         onClick={handleFormClick}
+//         className="cursor-pointer bg-marine-blue px-4 py-2 text-White hover:bg-purplish-blue"
+//       >
+//         {step === 4 ? 'Confirm' : 'Next Step'}
+//       </button>
+//     </footer>
+//   );
+// }
 
 export default Footer;
