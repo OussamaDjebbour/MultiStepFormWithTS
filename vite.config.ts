c725d9c4e4a1b8e4f6796b63a9cd193da34a1766
OussamaDjebbour <oussama_djebbour@outlook.com>
2024-08-05 23:57:56 +0100

    fix deployement issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), vue()],
+  build: {
+    outDir: 'dist',
+  },
+  publicDir: 'public', // Ensure this is correct
 });
